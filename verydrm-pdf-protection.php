5978656a6324190eb484bb3b52975219a048507a
VeryDRM <verydrm@gmail.com>
2026-07-08 15:06:37 +0100

    Update verydrm-pdf-protection.php

--- verydrm-pdf-protection.php
+++ verydrm-pdf-protection.php
@@ -2,8 +2,10 @@
 /**
  * Plugin Name: VeryDRM PDF Protection for WordPress
  * Plugin URI: https://verydrm.com/
- * Description: Protect PDF files in WordPress using VeryDRM DRM technology.
+ * Description: Protect PDF files in WordPress using VeryDRM.
  * Version: 0.1.0
+ * Requires at least: 6.0
+ * Requires PHP: 7.4
  * Author: VeryDRM
  * Author URI: https://verydrm.com/
  * License: GPL-2.0-or-later
@@ -14,55 +16,10 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-define('VERYDRM_PLUGIN_PATH', plugin_dir_path(__FILE__));
-define('VERYDRM_PLUGIN_URL', plugin_dir_url(__FILE__));
+define('VERYDRM_VERSION', '0.1.0');
+define('VERYDRM_PATH', plugin_dir_path(__FILE__));
+define('VERYDRM_URL', plugin_dir_url(__FILE__));
 
-require_once VERYDRM_PLUGIN_PATH . 'includes/class-api-demo.php';
+require_once VERYDRM_PATH . 'includes/class-loader.php';
 
-add_action('admin_menu', function () {
-
-    add_menu_page(
-        'VeryDRM',
-        'VeryDRM',
-        'manage_options',
-        'verydrm',
-        'verydrm_admin_page',
-        'dashicons-lock'
-    );
-
-});
-
-function verydrm_admin_page()
-{
-?>
-<div class="wrap">
-
-<h1>VeryDRM PDF Protection for WordPress</h1>
-
-<p>Welcome to VeryDRM.</p>
-
-<p>This plugin is currently under development.</p>
-
-<p>
-Future versions will support:
-
-<ul>
-
-<li>PDF DRM Protection</li>
-
-<li>Dynamic Watermark</li>
-
-<li>WooCommerce</li>
-
-<li>Paid Memberships Pro</li>
-
-<li>LearnDash</li>
-
-</ul>
-
-</p>
-
-</div>
-
-<?php
-}
+VeryDRM_Loader::run();
