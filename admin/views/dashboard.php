36631312926d16071328873bdb450976f402f638
VeryDRM <verydrm@gmail.com>
2026-07-08 15:07:39 +0100

    Create dashboard.php

---
+++ admin/views/dashboard.php
@@ -0,0 +1,11 @@
+<div class="wrap">
+
+<h1>VeryDRM PDF Protection for WordPress</h1>
+
+<p>Version 0.1.0</p>
+
+<p>The plugin has been installed successfully.</p>
+
+<p>Development is in progress.</p>
+
+</div>
