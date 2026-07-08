589b96bad47ea168784484a9639c7d9fedf3b6a7
VeryDRM <verydrm@gmail.com>
2026-07-08 15:03:29 +0100

    Create class-api-demo.php

---
+++ includes/class-api-demo.php
@@ -0,0 +1,17 @@
+<?php
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+class VeryDRM_API_Demo
+{
+
+    public function upload($email, $pdfFile)
+    {
+        // TODO
+
+        return true;
+    }
+
+}
